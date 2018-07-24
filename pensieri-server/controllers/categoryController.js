5aaf9d8a75c2aec8877143f237170a27c23b8ec5
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-07-23 20:09:54 -0700

    Updated with CRUD logic routes.

--- pensieri-server/controllers/categoryController.js
+++ pensieri-server/controllers/categoryController.js
@@ -1,8 +1,82 @@
+const Category = require('../models/Category');
+
 module.exports = {
-  //Get Index
-  getIndex: function(req, res) {
-    res.status(200).json({
-      message: "Welcome to the backend!"
+  //Get All Categories
+  getCategories: function(req, res) {
+    Category.find({}, (err, data) => {
+      if(err) {
+        console.error(err);
+        res.status(404).json({
+          message: err
+        });
+      } else {
+        if(data == null) {
+          res.status(400).json({
+            message: "No categories were found."
+          });
+        } else {
+          res.json(data);
+        }
+      }
+    });
+  },
+
+  //Create New Category
+  createCategory: function(req, res) {
+    var newCategoryData = new Category({
+      title: req.body.title,
+      description: req.body.description,
+      isPublished: req.body.isPublished
+    });
+    newCategoryData.save((err, data) => {
+      if(err) {
+        console.log("Error occurred while attempting to create a new category.");
+        res.status(404).json({
+          message: err
+        });
+      } else {
+        res.status(200).json(data);
+      }
+    });
+  },
+
+  //Update Category
+  updateCategory: function(req, res) {
+    Category.findByIdAndUpdate(req.params.id, { $set:
+      {
+        title: req.body.title,
+        description: req.body.description
+      }
+    }, { new: true }, (err, data) => { //New:true is so data returns the modified document. By default it's false.
+      if(err) {
+        console.log("Error occurred while attempting to update category with id [" + req.params.id + "]");
+        res.status(404).json({
+          message: err
+        });
+      } else {
+        res.status(200).json(data);
+      }
+    });
+  },
+
+  //Delete Category
+  deleteCategory: function(req, res) {
+    Category.findByIdAndRemove({ _id: req.params.id }, (err, data) => {
+      if(err) {
+        console.error(err);
+        res.status(404).json({
+          message: err
+        });
+      } else {
+        if(data == null) {
+          res.status(204).json({
+            message: "Failed to delete the requested category. The category was not found."
+          });
+        } else {
+          console.log("The category [${req.params.id}] was successfully deleted.");
+          res.status(200).json(data);
+        }
+      }
     });
   }
 }
--- pensieri-server/controllers/postController.js
+++ pensieri-server/controllers/postController.js
@@ -1,8 +1,83 @@
+const Post = require('../models/Post');
+
 module.exports = {
-  //Get Index
-  getIndex: function(req, res) {
-    res.status(200).json({
-      message: "Welcome to the backend!"
+  //Get All Posts
+  getPosts: function(req, res) {
+    Post.find({}, (err, data) => {
+      if(err) {
+        console.error(err);
+        res.status(404).json({
+          message: err
+        });
+      } else {
+        if(data == null) {
+          res.status(400).json({
+            message: "No posts were found."
+          });
+        } else {
+          res.json(data);
+        }
+      }
+    });
+  },
+
+  //Create New Post
+  createPost: function(req, res) {
+    var newPostData = new Post({
+      title: req.body.title,
+      body: req.body.body,
+      isPublished: req.body.isPublished,
+      isFeatured: req.body.isFeatured
+    });
+    newPostData.save((err, data) => {
+      if(err) {
+        console.log("Error occurred while attempting to create a new post.");
+        res.status(404).json({
+          message: err
+        });
+      } else {
+        res.status(200).json(data);
+      }
+    });
+  },
+
+  //Update Post
+  updatePost: function(req, res) {
+    Post.findByIdAndUpdate(req.params.id, { $set:
+      {
+        title: req.body.title,
+        description: req.body.description
+      }
+    }, { new: true }, (err, data) => { //New:true is so data returns the modified document. By default it's false.
+      if(err) {
+        console.log("Error occurred while attempting to update post with id [" + req.params.id + "]");
+        res.status(404).json({
+          message: err
+        });
+      } else {
+        res.status(200).json(data);
+      }
+    });
+  },
+
+  //Delete Post
+  deletePost: function(req, res) {
+    Post.findByIdAndRemove({ _id: req.params.id }, (err, data) => {
+      if(err) {
+        console.error(err);
+        res.status(404).json({
+          message: err
+        });
+      } else {
+        if(data == null) {
+          res.status(204).json({
+            message: "Failed to delete the requested post. The post was not found."
+          });
+        } else {
+          console.log("The post [${req.params.id}] was successfully deleted.");
+          res.status(200).json(data);
+        }
+      }
     });
   }
 }
