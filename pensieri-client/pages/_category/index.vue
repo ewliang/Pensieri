ce839926379895d02ebcbfa43ef647e13b3b37d7
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-07-28 11:22:15 -0700

    Initial commit.

---
+++ pensieri-client/pages/_category/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData () {
+    let post = await axios.get('/categories/')
+    return {
+      post: post.data
+    }
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pensieri-client/store/index.js
@@ -0,0 +1,7 @@
+import Vuex from 'vuex'
+
+const createStore = () => {
+  return new Vuex.Store({
+    
+  })
+}
