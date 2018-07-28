5a1136b81ef0c55641080d3dc866a25001fe4c2b
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-07-28 12:19:55 -0700

    Initial commit.

---
+++ pensieri-client/pages/admin/dashboard/categories/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData () {
+    let categories = await axios.get('/categories')
+    return {
+      categories: categories.data
+    }
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pensieri-client/pages/admin/dashboard/posts/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData () {
+    let posts = await axios.get('/posts')
+    return {
+      posts: posts.data
+    }
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
