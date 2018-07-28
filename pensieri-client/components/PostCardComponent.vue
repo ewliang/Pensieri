77f56ada32aef5cb8d2680a4b7af7b691715cdca
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-07-28 11:37:21 -0700

    Initial commit.

---
+++ pensieri-client/components/PostCardComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <article>
+    <img src = "" :alt = postTitle>
+    <h2>{{ postTitle }}</h2>
+    <p>
+      {{ postExcerpt }}
+    </p>
+  </article>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  props: [
+    'featuredImage', 'postTitle', 'postExcerpt', 'postURL'
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
