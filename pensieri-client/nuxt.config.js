61e8a5a71ff8f226af159b3b2862fe3dcd8c7d28
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-08-11 16:13:19 -0700

    Title change.

--- pensieri-client/nuxt.config.js
+++ pensieri-client/nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'pensieri-client',
+    title: 'Pensieri Client',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
