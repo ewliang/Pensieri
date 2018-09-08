a23dc4eb774198c5b5e69217580a0a4a50f2bf5e
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-08 13:42:54 -0700

    Created basic template and code for processing the creation of a simple blog post entry via POST.

--- pensieri-client/pages/admin/dashboard/posts/create.vue
+++ pensieri-client/pages/admin/dashboard/posts/create.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
+    <form @submit.prevent = "createPost()">
+      <label for = "postTitle">Title</label>
+      <input type = "text" v-model = "Post.title" placeholder = "Post Title" id = "postTitle">
+      <br>
+      <label for = "isFeatured">Is Featured?</label>
+      <select v-model = "Post.isFeatured" id = "isFeatured">
+        <option value = "false">False</option>
+        <option value = "true">True</option>
+      </select>
+      <label for = "isPublished">Is Published?</label>
+      <select v-model = "Post.isPublished" id = "isPublished">
+        <option value = "false">False</option>
+        <option value = "true">True</option>
+      </select>
+      <br>
+      <textarea v-model = "Post.body" placeholder = "Your post content..."></textarea>
+      <br>
+      <input type = "submit" value = "Create">
+    </form>
   </div>
 </template>
 
@@ -7,8 +26,26 @@
 import axios from 'axios'
 
 export default {
-  async asyncData () {
-    let posts = await axios.get('http://localhost:4000/posts')
+  data () {
+    return {
+      Post: {
+        title: '',
+        body: '',
+        isPublished: false,
+        isFeatured: false
+      }
+    }
+  },
+  methods: {
+    createPost () {
+      axios.post('http://localhost:4000/posts', this.Post)
+      .then(function (res) {
+        console.log(res)
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+    }
   }
 }
 </script>
