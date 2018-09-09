fa5dd04c4e34ab148e657cfd1d3669fbdfd41ab8
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-09 11:38:46 -0700

    Added code to display categories with delete button.

--- pensieri-client/pages/admin/dashboard/categories/index.vue
+++ pensieri-client/pages/admin/dashboard/categories/index.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Select</th>
+          <th>Title</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for = "(category, index) in categories" v-bind:key = "index">
+          <td><input type = "checkbox" v-bind:value = "category._id"></td>
+          <td><nuxt-link :to = "'/admin/dashboard/categories/edit/' + category.permalink">{{ category.title }}</nuxt-link></td>
+          <td><button @click.prevent = "deleteCategory(category._id)">Delete</button></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -13,9 +29,9 @@ export default {
       categories: categories.data
     }
   },
-  data () {
-    return {
-
+  methods: {
+    deleteCategory (categoryID) {
+      axios.delete('http://localhost:4000/categories/' + categoryID)
     }
   }
 }
