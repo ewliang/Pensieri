dd60e80b81187d4e83af4862b119796628f8230f
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-12-15 21:44:58 -0800

    Removed default boilerplate code.

--- pensieri-client/pages/index.vue
+++ pensieri-client/pages/index.vue
@@ -7,16 +7,6 @@
       <h2 class="subtitle">
         A Nuxt.js client for Pensieri. Pensieri is a full stack Node.js based blogging platform.
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
