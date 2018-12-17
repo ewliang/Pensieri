b4bed0a4b44a1caede9ac4acabae0f380a0a7ffb
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-12-16 23:47:33 -0800

    Removed axios implementation.

--- pensieri-client/pages/_category/index.vue
+++ pensieri-client/pages/_category/index.vue
@@ -1,29 +1,29 @@
 <template>
   <div>
-    <PostCardComponent v-for = "(post, index) in posts"
+    TODO: Get posts by category filter.
+    <PostCardComponent v-for = "(post, index) in postsByCategory"
       v-bind:key = "index"
       v-bind:postTitle = "post.title"
-      v-bind:postExcerpt = "post.excerpt"
+      v-bind:postExcerpt = "post.body"
       v-bind:postURL = "post.permalink">
     </PostCardComponent>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import postsByCategory from '~/apollo/queries/public/postsByCategory'
 import PostCardComponent from '~/components/PostCardComponent.vue'
 
 export default {
-  async asyncData ({ params }) {
-    // Need to get the category ID somehow... currently passing title which is not the right parameter data type.
-    let posts = await axios.get('http://localhost:4000/posts/' + params.category)
-    return {
-      posts: posts.data
-    }
+  components: {
+    PostCardComponent
   },
-  data () {
-    return {
-
+  apollo: {
+    postsByCategory: {
+      query: postsByCategory,
+      variables: {
+        category: 'miscellaneous'
+      }
     }
   }
 }
