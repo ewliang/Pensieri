ac51baf09c6f7b1470cbd45c24737d4c106e425e
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-12-16 23:47:21 -0800

    Removed axios implementation and test data.

--- pensieri-client/pages/admin/dashboard/posts/index.vue
+++ pensieri-client/pages/admin/dashboard/posts/index.vue
@@ -10,35 +10,19 @@
       </thead>
       <tbody>
         <tr v-for = "(post, index) in posts" v-bind:key = "index">
-          <td><input type = "checkbox" v-bind:value = "post._id"></td>
-          <td><nuxt-link :to = "'/admin/dashboard/posts/edit/' + post._id">{{ post.title }}</nuxt-link></td>
-          <td><button @click.prevent = "deletePost(post._id)">Delete</button></td>
+          <td><input type = "checkbox" v-bind:value = "post.id"></td>
+          <td><nuxt-link :to = "'/admin/dashboard/posts/edit/' + post.id">{{ post.title }}</nuxt-link></td>
+          <td><button @click.prevent = "deletePost(post.id)">Delete</button></td>
         </tr>
       </tbody>
     </table>
-    <br>
-    <p>TEST</p>
-    <ul>
-      <li v-for = "p in posts" :key = "p.id">
-        {{ p.title }}
-        {{ p.id }}
-      </li>
-    </ul>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 import posts from '~/apollo/queries/admin/posts'
 
 export default {
-  async asyncData () {
-    /*
-    let posts = await axios.get('http://localhost:4000/posts')
-    return {
-      posts: posts.data
-    }*/
-  },
   apollo: {
     posts: {
       query: posts
