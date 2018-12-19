076523367c39384dd8b99e234329df5d851d6c04
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-12-19 10:52:51 -0800

    Added NavBarComponent.

--- pensieri-client/layouts/default.vue
+++ pensieri-client/layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <NavbarComponent/>
     <nuxt/>
   </div>
 </template>
+
+<script>
+import NavbarComponent from '~/components/NavbarComponent.vue'
+
+export default {
+  components: {
+    NavbarComponent
+  }
+}
+</script>
 
 <style>
 </style>
