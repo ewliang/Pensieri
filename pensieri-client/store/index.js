9782646faafd9626a76f1be437172b9098687ca5
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2019-08-15 23:01:26 -0700

    Added getPosts action

--- pensieri-client/store/index.js
+++ pensieri-client/store/index.js
@@ -1,7 +1,29 @@
 import Vuex from 'vuex'
+import postsByCategory from '~/apollo/queries/public/postsByCategory'
 
-const createStore = () => {
+
+
+const store = () => {
   return new Vuex.Store({
-    
+    actions: {
+      getPosts: (store, category) => {
+        
+        this.$apollo.queries({
+          query: postsByCategory,
+          variables() {
+            return {
+              category: category
+            }
+          }
+        }).then(data => {
+          console.log(data)
+        }).catch(error => {
+          console.error(error)
+        })
+        
+      }
+    }
   })
 }
+
+export default store
